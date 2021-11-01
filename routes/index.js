8c4944a4b3a9f37d63ea324bff3a5f691b4f42fd
Mateusz <mateuszzygadlo00@gmail.com>
2021-11-01 14:05:23 +0100

    Adding page without hosting

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,31 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const dayjs = require('dayjs')
 
-/* GET home page. */
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: dayjs(new Date()).format('DD/MM/YYYY')
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: dayjs(new Date()).format('DD/MM/YYYY')
+  }
+];
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { messages: messages });
+});
+
+router.post('/new', function(req, res, next) {
+  messages.push({
+    text: req.body.content,
+    user: req.body.author,
+    added: dayjs(new Date()).format('DD/MM/YYYY'),
+  })
+  res.redirect('/');
 });
 
 module.exports = router;
